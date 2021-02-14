1d0310da6145ec675af3433ad2884668cc8dcdae
Jaime-Pinkman <marathon.our@gmail.com>
2021-02-15 01:33:08 +0300

    Added face_comparator.py and edited check view

--- images/views.py
+++ images/views.py
@@ -2,49 +2,31 @@ from rest_framework.response import Response
 from images.models import Photo
 from rest_framework.views import APIView
 from images.face_recognizer import FaceRecognizer
-from scipy.spatial import distance
+from images.face_comparator import FaceComparator
 
 
-"""class CheckImageView(APIView):
+class CheckImageView(APIView):
     def post(self, request):
         file = request.data.get('file', None)
         if file:
-            image = Photo.objects.create(image=file)
-            face = find_save_face(image.image.path)
-            image.delete()
-            if face is not None:
-                face_thresh = 0.6
-                start_point = 100
-                faces = Photo.objects.all()
-                index = -1
-                for i in range(len(faces)):
-                    if faces[i].portrait != '':
-                        dist = distance.euclidean(list(map(float, faces[i].portrait.split(','))), face.chosen["face_descriptor"])
-                        if dist < start_point:
-                            index = i
-                            start_point = dist
-                print(start_point)
-                if start_point < face_thresh:
-                    print(faces[index].image)
-                    return Response(status=200)
-                else:
-                    print("A New Face!")
-                    return Response(status=200)
-            else:
+            try:
+                face_rec = FaceRecognizer(file)
+            except Exception:
                 return Response(status=400)
-        else:
-            return Response(status=400)"""
+            faces = Photo.objects.all()
+            face_comp = FaceComparator(face_rec.portrait, faces)
+
+            return Response({'exists': face_comp.check_if_the_same_person()})
+
 
 
 class UploadView(APIView):
     def post(self, request):
         file = request.data.get('file', None)
         if file:
-            try:
-                user_frame = FaceRecognizer(file)
-            except Exception:
-                return Response(status=400)
-            Photo.objects.create(image=file, portrait=str(user_frame.portrait).replace("\n", ",")) #
+            face_rec = FaceRecognizer(file)
+
+            Photo.objects.create(image=file, portrait=str(face_rec.portrait).replace("\n", ",")) #
             return Response(status=200)
         else:
             return Response(status=400)
