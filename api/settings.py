95c55ebf3f32cf5e9629fbee057d1916164c95bf
Jaime-Pinkman <marathon.our@gmail.com>
2021-02-15 13:34:53 +0300

    put configs to dev.env

--- api/settings.py
+++ api/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
+from os import getenv as env
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,8 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'j($by7ln&5$k$97c@a1e2l6$*85ltdzwi61sdw1qe81byx-dyo'
-
+SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
